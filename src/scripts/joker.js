4e627002b7028e6cf355b859f32b778d363fd001
Alexander <alexanderhardeland@gmail.com>
2023-05-09 04:22:07 +0000

    Deleted the old code from former lesson as this was needed

--- src/scripts/joker.js
+++ src/scripts/joker.js
@@ -1,7 +1,4 @@
-import joker from "one-liner-joke";
 import joker from "give-me-a-joke";
-
-console.log(joker.getRandomJoke().body);
 
 export function dadJoke() {
   return new Promise(function(resolve, reject) {
