d476a82a60efdc162f8d654a628b537f831747d6
Alexander <alexanderhardeland@gmail.com>
2023-05-09 04:12:48 +0000

    Adds dadJoke function, returns a Promise. When promise is resolved it returns a joke

--- src/scripts/joker.js
+++ src/scripts/joker.js
@@ -1,2 +1,10 @@
 import joker from "one-liner-joke";
+import joker from "give-me-a-joke";
+
 console.log(joker.getRandomJoke().body);
+
+export function dadJoke() {
+  return new Promise(function(resolve, reject) {
+    joker.getRandomDadJoke(resolve);
+  })
+}
